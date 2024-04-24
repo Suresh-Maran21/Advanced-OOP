812bca9bf58f41713794e29e9344d307d470da9d
Suresh-Maran21 <166098760+Suresh-Maran21@users.noreply.github.com>
2024-04-25 01:38:33 +0530

    Abstraction

---
+++ Abstraction/AbstractOne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AbstractOne;
+class Program{
+    public static void Main(string[] args)
+    {
+        Cylinders cylinder = new Cylinders(7,10);
+        Cubes cube=new Cubes(10);
+
+        Console.WriteLine($"the area of the cylinder is {cylinder.CalculateArea()}");
+        Console.WriteLine($"the volumke of the cylinder is {cylinder.CalculateVolume()}");
+        Console.WriteLine($"the area of the cube is {cube.CalculateArea()}");
+        Console.WriteLine($"the volume of the cube is {cube.CalculateVolume()}");
+    }
+}
